5974142ae086668d6d2af5a4d18cfa4d50a5cc39
Florent Jouatte <fjouatte@anybox.fr>
2016-10-06 14:39:23 +0200

    display warning message when not able to disconnect from sso

--- auth_oauth2/controlers/session.py
+++ auth_oauth2/controlers/session.py
@@ -1,7 +1,11 @@
+# coding: utf-8
+
 import httplib2
+import logging
 from openerp.addons.web import http
 from openerp.addons.web.controllers.main import Session as session
 from openerp.tools import config
+from openerp.osv import osv
 
 openerpweb = http
 
@@ -16,9 +20,20 @@ class Session(session):
         id_token = req.session.model('res.users').read(
             uid, ['oauth_id_token']
         )['oauth_id_token']
+        exceptions = False
         if not id_token:
             req.session._suicide = True
+            return
         conn = httplib2.Http()
         uri = config.get('auth_oauth2.end_session_endpoint')
-        conn.request(uri, 'GET', body="{'id_token': %s}" % (id_token))
+        try:
+            conn.request(uri, 'GET', body="{'id_token': %s}" % (id_token))
+        except Exception as e:
+            logging.error("Oauth2 end session : %s" % (e.strerror))
+            exceptions = e
         req.session._suicide = True
+        if exceptions:
+            raise osv.except_osv(
+                u"Attention, une erreur s'est produite lors de la déconnexion",
+                u"Il est possible que vous ne soyez pas déconnecté du SSO"
+            )
