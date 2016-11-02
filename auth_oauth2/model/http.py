56e21c78ef11fe9fa01c057502c0e2f79104d17f
Florent Jouatte <fjouatte@anybox.fr>
2016-11-02 17:41:07 +0100

    add inactive session management

---
+++ auth_oauth2/model/http.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from openerp.addons.web import http
+import time
+_logger = logging.getLogger(__name__)
+
+
+def custom_session_gc(session_store):
+    deadline = time.time() - 60
+    for fname in os.listdir(session_store.path):
+        path = os.path.join(session_store.path, fname)
+        try:
+            if os.path.getmtime(path) < deadline:
+                os.unlink(path)
+            else:
+                os.utime(path, None)
+        except OSError:
+            pass
+
+http.session_gc = custom_session_gc
